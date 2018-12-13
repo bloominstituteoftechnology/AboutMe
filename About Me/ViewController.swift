1d2cd2ec5abb2e54f05babea97a0c806c8fcf2cd
sam <samuelfbiggs@gmail.com>
2018-12-12 21:22:53 -0700

    Sam Biggs Introduction MVP finished

---
+++ About Me/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Samuel Biggs on 9/30/18.
+//  Copyright © 2018 Samuel Biggs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func introduceMe(_ sender: Any) {
+        
+        let aboutMe = "Hello my name is sam"
+        let age = "I am 25 years old"
+        var favoriteColor = "My favorite color is Blue"
+        var favoriteFood = "I really love Chicken Wings"
+        print(aboutMe)
+        print(favoriteColor)
+        print(favoriteFood)
+        print(age)
+        
+    }
+    
+}
+
+
