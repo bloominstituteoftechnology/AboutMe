f6ba991b7656ba5ddba06f744a79e264ddc91b2c
fthmls <fthmls.one@gmail.com>
2020-02-11 12:58:18 -0500

    Create var myName

---
+++ AboutMe.playground/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ 
+ ## Instructions
+ 
+ * Create variables and constants for various parts about you, such as your name, your hometown, your favorite color and food, and anything else you think someone would be interested to know about you. Think about which of these should be variables, and which should be constants. Try to come up with at least 8 constants and variables in total.
+ 
+ * Print each of these using `print()` statements and inserting the variable or constant in the parentheses.
+ 
+ ### When completed, please zip up your code and upload it.
+
+ ---
+ 
+ ##### Bonus (this is optional):
+ 
+ * Create a constant called `introduction`. This should be a readable sentence or paragraph that includes all of the constants and variables you made and combines them into a single string.
+ 
+ *Hints:* Look up _string interpolation_ in Swift to learn how to turn variables into text. Google is your friend here. 😉
+ 
+ > We will cover string interpolation in detail in a later lesson, so don't worry if you don't already know it, or find it confusing at first.
+ 
+ */
+
+import UIKit
+
+var myName = "Jordan Proctor"
